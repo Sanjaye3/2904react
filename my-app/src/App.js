6995e4a54c7ee8bb7d0efb6e5aedc23f22521bdc
sanjaye3 <sanjayejayachandar@gmail.com>
2022-06-07 10:03:57 +0530

    theme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,14 +21,38 @@ import {useNavigate} from "react-router-dom";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+// import Paper from '@mui/material/Paper';
+import { Paper } from '@mui/material';
 <script src="js/reactjs/main.js" type = "text/babel"></script>
 
+
+
+
+
 export default function App(){
-  const INITIAL_MOVIE_LIST = [ { name: "RRR", poster: "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG", rating: 8.8, summary: "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.", }, { name: "Iron man 2", poster: "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg", rating: 7, summary: "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.", }, { name: "No Country for Old Men", poster: "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg", rating: 8.1, summary: "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.", }, { name: "Jai Bhim", poster: "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg", summary: "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case", rating: 8.8, }, { name: "The Avengers", rating: 8, summary: "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.", poster: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg", }, { name: "Interstellar", poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg", rating: 8.6, summary: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.", }, { name: "Baahubali", poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg", rating: 8, summary: "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.", }, { name: "Ratatouille", poster: "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=", rating: 8, summary: "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him." } ]; 
+
+  
+  const INITIAL_MOVIE_LIST = [ { id:"100",name: "RRR", poster: "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG", rating: 8.8, summary: "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.", }, { id:"101",name: "Iron man 2", poster: "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg", rating: 7, summary: "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.", }, { id:"102",name: "No Country for Old Men", poster: "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg", rating: 8.1, summary: "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.", }, { id:"103",name: "Jai Bhim", poster: "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg", summary: "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case", rating: 8.8, }, { id:"104",name: "The Avengers", rating: 8, summary: "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.", poster: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg", }, { id:"105",name: "Interstellar", poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg", rating: 8.6, summary: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.", }, { name: "Baahubali", poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg", rating: 8, summary: "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.", }, { name: "Ratatouille", poster: "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=", rating: 8, summary: "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him." } ]; 
+  fetch("https://6294c26d63b5d108c1933336.mockapi.io/movies")
+  .then(response => response.json)
+  .then(data => { 
+    INITIAL_MOVIE_LIST(data);
+  });
   const[movieList, setMovieList] = useState(INITIAL_MOVIE_LIST);
+  const[mode,setMode] = useState("dark")
+  const theme = createTheme({
+    palette: {  
+      mode: mode,
+    },
+  });
   const navigate = useNavigate();
   return (
-
+    
+    <ThemeProvider theme={theme}>
+    <Paper elevation={0} />
     
     <div>
       <AppBar position="static">
@@ -38,6 +62,12 @@ export default function App(){
           <Button color="inherit"  onClick={() => navigate('/')}>Welcome</Button>
           <Button color="inherit" onClick={() => navigate('/Movies')}>Movie List</Button>
           <Button color="inherit" onClick={() => navigate('/Movies/add')}>Add Movie</Button>
+          <Button 
+          startIcon={mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+          color="inherit" onClick={() => setMode(mode==="light"?"dark":"light")}>
+            
+          {mode==="light"?"dark":"light"} mode
+          </Button>
         </Toolbar>
       </AppBar>
 <div className="router-container">
@@ -52,6 +82,9 @@ export default function App(){
     </Routes>
     </div>
 </div>
+  <Paper />
+</ThemeProvider>
+
   )
   
 }
@@ -137,6 +170,8 @@ function Movie({movie}){
       
        
       <p style ={parastyle} className="movie-summary">{movie.summary}</p>
+
+       
       <CardActions>
       <Counter/> <IconButton onClick={() =>{ let copyMovieList = [...Movielist];
         copyMovieList.splice(movie.name,1);
